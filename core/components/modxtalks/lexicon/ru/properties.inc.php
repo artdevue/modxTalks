085da4586d183bb3626c2bfef16bc6fe1052e501
Viktor Matushevskyi <viktorminator@gmail.com>
2013-02-01 16:04:31 +0200

    added Properties (RU, UA)

---
+++ core/components/modxtalks/lexicon/ru/properties.inc.php
@@ -0,0 +1,26 @@
+<?php
+
+$_lang['modxtalks.bbcode'] = 'Включить обработку и редактор BBCode в комментариях? Да / Нет';
+$_lang['modxtalks.detectUrls'] = 'Переводить адреса веб ресурсов в комментариях в ссылки?';
+$_lang['modxtalks.editOptionsControls'] = 'Выберите теги, для отображения иконок редактора. Доступные - fixed, image, link, strike, header, italic, bold, video, quote';
+$_lang['modxtalks.smileys'] = 'Включить отображение смайликов в комментариях? Да / Нет';
+$_lang['modxtalks.ajax'] = 'Включить аякс обновление комментариев с сервера? Да / Нет';
+$_lang['modxtalks.commentLength'] = 'Максимально допустимая длина комментария в символах.';
+$_lang['modxtalks.commentsPerPage'] = 'Максимальное количество комментариев на странице для отображения при визуализации управления навигации по страницам.';
+$_lang['modxtalks.edit_time'] = 'Время, после которого пользователь не может редактировать комментарий';
+$_lang['modxtalks.jquery'] = 'Если вы используете своё подключение jQuery для странице, или используете сторонние сервисы, выбирите НЕТ, и убедитесь, что версия jQuery не ниже v1.7';
+$_lang['modxtalks.onlyAuthUsers'] = 'Оставлять комментарии могут только авторизованные пользователи.';
+$_lang['modxtalks.moderator'] = 'Укажите через запятую группы модераторов, которые могут модерировать комментарии';
+$_lang['modxtalks.mtDateFormat'] = 'Формат даты отображаемый в комментариях.';
+$_lang['modxtalks.mtGravator'] = 'Включить граватар в сообщениях пользователей. Да / Нет';
+$_lang['modxtalks.defaultAvatar'] = 'Полная ссылка на аватар, отображается при отсутствии аватара в системе Граватар.';
+$_lang['modxtalks.mtgravatarSize'] = 'Размер граватара выводимого в комментариях.
+Премодерация комментариев';
+$_lang['modxtalks.preModarateComments'] = 'Если установленно - Да, то комментарии изначально попадают на промодерацию. После потверждения менеджером, комментарий выводиться на сайте.';
+$_lang['modxtalks.scrubberTop'] = 'Отоброжения скрайбера в верхней позиции сообщений, по умолчанию (НЕТ), скрайбер отображаеться в правой колонке.';
+$_lang['modxtalks.scrubberOffsetTop'] = 'Отступ скрайбера от вехней части экрана при скроле. Это нужно, если у вас присутствуют элементы с позицированием fixed, например меню';
+$_lang['modxtalks.highlight'] = 'Включить подсветку кода Highlight в комментариях? Да / Нет
+';
+$_lang['modxtalks.highlighttheme'] = 'Доступные темы: Default, Dark, FAR, IDEA, Sunburst, Zenburn, Visual Studio, Ascetic, Magula, GitHub, Google Code, Brown Paper, School Book, IR Black, Solarized-Dark, Solarized-Light, Arta, Monokai, XCode, Pojoaque, Rainbow, Tomorrow, Tomorrow Night, Tomorrow Night Bright, Tomorrow Night Blue, Tomorrow Night Eighties';
+$_lang['modxtalks.emailsFrom'] = 'Адрес электронной почты, для указания отправителя в заголовке письма';
+$_lang['modxtalks.emailsReplyTo'] = 'Адрес электронной почты для ответа';
