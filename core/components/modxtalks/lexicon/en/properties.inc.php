07f9fd0463fda1f585a874c53847ab3533fa5a1a
Viktor Matushevskyi <viktorminator@gmail.com>
2013-02-01 15:54:49 +0200

    added Properties (english)

---
+++ core/components/modxtalks/lexicon/en/properties.inc.php
@@ -0,0 +1,26 @@
+<?php
+
+$_lang['modxtalks.bbcode'] = 'Set on BBCode editor in comments?';
+$_lang['modxtalks.detectUrls'] = 'Переводить адреса веб ресурсов в комментариях в ссылки?';
+$_lang['modxtalks.editOptionsControls'] = 'Choose control tags for editor. Available - fixed, image, link, strike, header, italic, bold, video, quote';
+$_lang['modxtalks.smileys'] = 'Enable smileys in comments? Yes/No';
+$_lang['modxtalks.ajax'] = 'AJAX update of comments. Enable realtime update of comments from server? Yes/No';
+$_lang['modxtalks.commentLength'] = 'Comment length. Max length of comments (in symbols).';
+$_lang['modxtalks.commentsPerPage'] = 'Max number of comments on page for pagination.';
+$_lang['modxtalks.edit_time'] = 'Time for editing. Time after which user can`t edit his comment
+';
+$_lang['modxtalks.jquery'] = 'If you are using jQuery Lib on your page choose NO, but make sure your version of jQuery is 1.7+';
+$_lang['modxtalks.onlyAuthUsers'] = 'Only authorised users. Comments can be left only by authorised users.';
+$_lang['modxtalks.moderator'] = 'Moderator groups separated by commas.';
+$_lang['modxtalks.mtDateFormat'] = 'Date format j-m-Y, G:i	Date format displayed in a comment.';
+$_lang['modxtalks.mtGravator'] = 'Use gravatar Enable gravatar image in user messages. Yes/No';
+$_lang['modxtalks.defaultAvatar'] = 'Default avatar Full link to avatar which appeared when Gravatar is disabled or doesn`t exist.';
+$_lang['modxtalks.mtgravatarSize'] = 'Gravatar size in the comment.';
+$_lang['modxtalks.preModarateComments'] = 'Comments premoderation If YES then comments are premoderated. After approval comment will appear on the site.';
+$_lang['modxtalks.scrubberTop'] = 'Displaying scrubber in top position of the comment. By defaults NO - right column.
+';
+$_lang['modxtalks.scrubberOffsetTop'] = 'Scrubber offset from top position when scrolling. This is needed when you have elements with fixed postion, e.g. menu.';
+$_lang['modxtalks.highlight'] = 'Enable code highlight in the comments? Yes/No';
+$_lang['modxtalks.highlighttheme'] = 'GitHub Hightlight theme for code. Available themes: Default, Dark, FAR, IDEA, Sunburst, Zenburn, Visual Studio, Ascetic, Magula, GitHub, Google Code, Brown Paper, School Book, IR Black, Solarized-Dark, Solarized-Light, Arta, Monokai, XCode, Pojoaque, Rainbow, Tomorrow, Tomorrow Night, Tomorrow Night Bright, Tomorrow Night Blue, Tomorrow Night Eighties';
+$_lang['modxtalks.emailsFrom'] = 'Email for sender of message.';
+$_lang['modxtalks.emailsReplyTo'] = 'Email for answer';
