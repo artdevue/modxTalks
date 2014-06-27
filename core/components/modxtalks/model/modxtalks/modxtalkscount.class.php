557ef4260b627898aa1f17ca60f6503b6d113af7
Брежнев Иван <brezhnev.ivan@yahoo.com>
2014-06-27 12:46:39 +0300

    Fix error with empty IN condition

--- core/components/modxtalks/model/modxtalks/modxtalkscount.class.php
+++ core/components/modxtalks/model/modxtalks/modxtalkscount.class.php
@@ -15,9 +15,9 @@ class modxTalksCount {
     /** @var array $config */
     public $config = array();
 
-    function __construct(modX &$modx,array $config = array()) {
+    function __construct(modX & $modx,array $config = array()) {
         $this->modx =& $modx;
-        $this->config = array_merge(array(),$config);
+        $this->config = array_merge(array(), $config);
     }
 
     /**
@@ -34,15 +34,16 @@ class modxTalksCount {
             $lisrArray = $mt_list[1];
             array_walk($lisrArray, 'trim');
             // Divide into two arrays, one for the id of the resource, the other by conversation name
-            foreach ($lisrArray as $key=>$value){
+            foreach ($lisrArray as $key => $value){
                 if ($value{0} == 'r') {
                     $r[$key] = substr($value, 2);
                 } else {
                     $c[$key] = substr($value, 2);
                 }
             }
+
             // If the array is not empty, choose the number of comments on the resource id (column rid)
-            if (is_array($r)) {
+            if ($r && is_array($r)) {
                 array_walk($r, 'intval');
                 $rr = $this->modx->newQuery('modxTalksConversation', array('rid:IN' => $r));
                 if ($rr->prepare() && $rr->stmt->execute()) {
@@ -57,24 +58,22 @@ class modxTalksCount {
 
             }
 
-foreach ($r as $n) {
-    if ($k = array_search('r_'.$n, $mt_list[1], true)) {
-        $mt_list[1][$k] = 0;
-    }
-}
-            //echo '<pre>';
-            //print_r($mt_list[1]);
-            //print_r($r);die;
+            foreach ($r as $n) {
+                if ($k = array_search('r_' . $n, $mt_list[1], true)) {
+                    $mt_list[1][$k] = 0;
+                }
+            }
+
             // If the array is not empty, choose the number of comments on the conversation name (column conversation)
-            if (is_array($c)) {
+            if ($c && is_array($c)) {
                 $rr = $this->modx->newQuery('modxTalksConversation', array('conversation:IN' => $c));
                 if ($rr->prepare() && $rr->stmt->execute()) {
                     $results = $rr->stmt->fetchAll(PDO::FETCH_ASSOC);
                     foreach ($results as $resultr) {
                         $objMt = json_decode($resultr['modxTalksConversation_properties']);
-                         if (in_array('c_'.$resultr['modxTalksConversation_conversation'],$lisrArray)) {
-                            $mt_list[1][array_search('c_'.$resultr['modxTalksConversation_conversation'],$lisrArray)] =  $objMt->comments->total;
-                         }
+                        if (in_array('c_'.$resultr['modxTalksConversation_conversation'],$lisrArray)) {
+                           $mt_list[1][array_search('c_'.$resultr['modxTalksConversation_conversation'],$lisrArray)] =  $objMt->comments->total;
+                        }
                     }
                 }
 
@@ -82,6 +81,7 @@ foreach ($r as $n) {
             // Replace all your templates in the resource to the correct values
             $output = str_replace($mt_list[0], $mt_list[1], $output);
         }
+
         return true;
     }
 }
